bb5d29a79f3f1b2dfd13d849e0ba81fa35a2cb3a
AS81 <sanloginchik@gmail.com>
2021-08-17 21:03:52 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  solveTheSudoku(matrix);
+  return matrix;
+};
+
+function solveTheSudoku(m) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (m[i][j] == 0) {
+        for (let n = 1; n < 10; n++) {
+          if (testCell(m, i, j, n)) {
+            m[i][j] = n;
+            if (solveTheSudoku(m)) {
+              return true;
+            } else {
+              m[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function testCell(cellSudoku, row, col, num) {
+  //let rowCell = (colCell = 0);
+  for (let i = 0; i < 9; i++) {
+    let rowCell = Math.floor(row / 3) * 3 + Math.floor(i / 3);
+    let colCell = Math.floor(col / 3) * 3 + (i % 3);
+    if (
+      cellSudoku[row][i] == num ||
+      cellSudoku[i][col] == num ||
+      cellSudoku[rowCell][colCell] == num
+    ) {
+      return false;
+    }
+  }
+  return true;
 }
